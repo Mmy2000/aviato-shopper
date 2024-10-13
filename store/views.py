242d65a43914652a279acedba85e66ff9da94564
Mmy2000 <yahianada005@gmail.com>
2024-10-14 02:43:46 +0300

    display products in store page

--- store/views.py
+++ store/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product , Brand
+from category.models import Category , Subcategory
 # Create your views here.
 
 def product_list(request):
 
     products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
     context = {
-        'products':products
+        'products':products,
+        'product_count':product_count
     }
 
     return render(request , 'products/product_list.html' , context)
