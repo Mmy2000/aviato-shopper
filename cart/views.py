009f5c39f92fdd55947a8fdec87bc7cf5dbfdceb
Mmy2000 <yahianada005@gmail.com>
2024-10-15 05:30:59 +0300

    create cart function

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from store.models import Product
 from .models import Cart , CartItem
+from django.core.exceptions import ObjectDoesNotExist
+
 # Create your views here.
 
 
@@ -25,10 +27,26 @@ def add_to_cart(request , product_id):
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(product = product , cart = cart , quantity = 1)
         cart_item.save()
-        
+
     return redirect('cart')
 
 
 
-def cart(request):
-    return render(request , 'cart.html')
+def cart(request, total = 0 , quantity = 0 , cart_items = None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart = cart , is_active = True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+
+    }
+
+    return render(request , 'cart.html' , context)
