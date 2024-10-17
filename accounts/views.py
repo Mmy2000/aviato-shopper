9dc41483fcdad2b7d2379fd07e86aa6beed1f2a7
Mmy2000 <yahianada005@gmail.com>
2024-10-17 22:27:18 +0300

    handle the cart when the user login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render , redirect , get_object_or_404
+
+from cart.cart_utils import _cart_id
+from cart.models import Cart, CartItem
 from .forms import RegistrationForm , UserForm , ProfileForm
 from .models import User , Profile
 from django.contrib import messages, auth
@@ -59,6 +62,17 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter( cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass    
             auth.login(request, user)
             messages.success(request, 'Loged in Successfully')
             return redirect('profile')
